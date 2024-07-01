6df86f2c1cbb7d9ac570d62e435a456c01768604
ENZO MAKENZY DE QUEIROZ BEZERRA <a92855484@rn.senac.local>
2024-07-01 11:10:01 -0300

    Adicionando os cursos e os comandos para vários carroséis

--- js/home-mobile.js
+++ js/home-mobile.js
@@ -1,36 +1,42 @@
-const slides = document.querySelectorAll(".slide");
-const circles = document.querySelectorAll(".circle");
-let currentSlide = 0;
+// Seleciona todos os carrosséis na página
+const carousels = document.querySelectorAll('.carousel');
 
-function showSlide(index) {
-  slides.forEach((slide, i) => {
-    if (i === index) {
-      slide.classList.add("active");
-    } else {
-      slide.classList.remove("active");
-    }
+// Para cada carrossel, define variáveis e eventos
+carousels.forEach((carousel, index) => {
+  const slides = carousel.querySelectorAll('.slide');
+  const circles = carousel.querySelectorAll('.circle');
+  let currentSlide = 0;
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      if (i === index) {
+        slide.classList.add('active');
+      } else {
+        slide.classList.remove('active');
+      }
+    });
+
+    circles.forEach((circle, i) => {
+      if (i === index) {
+        circle.style.background = '#005594';
+        circle.style.color = '#005594';
+      } else {
+        circle.style.background = '';
+        circle.style.color = '';
+      }
+    });
+  }
+
+  carousel.querySelector('#prevButton').addEventListener('click', function() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
   });
 
-  circles.forEach((circle, i) => {
-    if (i === index) {
-      circle.style.background = '#005594';
-      circle.style.color = '#005594';
-    } else {
-      circle.style.background = '';
-      circle.style.color = '';
-    }
+  carousel.querySelector('#nextButton').addEventListener('click', function() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
   });
-}
 
-document.getElementById("prevButton").addEventListener("click", function() {
-  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  // Mostra o slide inicial
   showSlide(currentSlide);
 });
-
-document.getElementById("nextButton").addEventListener("click", function() {
-  currentSlide = (currentSlide + 1) % slides.length;
-  showSlide(currentSlide);
-});
-
-    
-showSlide(currentSlide);
